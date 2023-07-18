cb827f8083865b49ebacefda574eaee3e51af344
Kelsey <37933054+kelman7000@users.noreply.github.com>
2023-07-18 15:00:37 -0600

    added the necessary views

---
+++ COMP_482_Assignment_03/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_482_Assignment_03.ViewModels
+{
+	public class MainWindowViewModel
+	{
+
+	}
+}
