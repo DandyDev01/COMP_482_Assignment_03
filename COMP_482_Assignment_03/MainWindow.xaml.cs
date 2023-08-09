548866a48365d477d2b6e264f92efc3f4b7bd986
Kelsey <37933054+kelman7000@users.noreply.github.com>
2023-08-08 20:45:54 -0600

    started working on the timer for re-authenticating

--- COMP_482_Assignment_03/MainWindow.xaml.cs
+++ COMP_482_Assignment_03/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace COMP_482_Assignment_03
 {
@@ -20,9 +22,23 @@ namespace COMP_482_Assignment_03
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		private readonly DispatcherTimer timer;
+		private bool isRunning = true;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			timer = new DispatcherTimer(DispatcherPriority.ApplicationIdle);
+			timer.Interval = TimeSpan.FromSeconds(30);
+			timer.Tick += TimerTick;
+			timer.Start();
+		}
+
+		private void TimerTick(object? sender, EventArgs e)
+		{
+			MessageBox.Show("timer: ", "re-athenticate", MessageBoxButton.OK, MessageBoxImage.Information);
 		}
 	}
 }
